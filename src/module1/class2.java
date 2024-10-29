b41302e5a8af3fa16f5169dccca9b1dfea5f0809
brahmini-tech <brahminiyadav777@gmail.com>
2024-10-29 07:33:49 +0000

    Create class2.java

---
+++ src/module1/class2.java
@@ -0,0 +1,7 @@
+public class class2 {
+
+    public static void main(String[] args) {
+        System.out.println("Class ");
+        System.out.println("Additional code changes in Clkass 1");
+    }
+}
