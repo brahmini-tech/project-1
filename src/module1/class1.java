01c2e39bfaa37f602c1ad505b7183099e2ed6e0a
brahmini-tech <brahminiyadav777@gmail.com>
2024-10-29 07:37:07 +0000

    Update class1.java
    code updated for class 1

--- src/module1/class1.java
+++ src/module1/class1.java
@@ -4,6 +4,6 @@ public class class1 {
 
     public static void main(String[] args) {
         System.out.println("Class 1");
-        System.out.println("Additional code changes in Clkass 1");
+        System.out.println("Additional code changes in Class 1");
     }
 }
