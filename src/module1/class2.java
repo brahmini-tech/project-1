619609660beceabba1952647f2a985be8c6d1365
brahmini-tech <brahminiyadav777@gmail.com>
2024-10-29 07:37:41 +0000

    Update class2.java
    Code updated in Class 2

--- src/module1/class2.java
+++ src/module1/class2.java
@@ -1,7 +1,7 @@
 public class class2 {
 
     public static void main(String[] args) {
-        System.out.println("Class ");
-        System.out.println("Additional code changes in Clkass 1");
+        System.out.println("Class 2");
+        System.out.println("Additional code changes in Class 2");
     }
 }
